63f289e8bda2d07117ea633865ea02423be479c1
Soufiane El Kharmoudi <soufiane.elkharmoudi@swissohm.ch>
2022-11-02 09:49:59 +0100

    modification

--- src/Account_projet_poo/PetitMenu.java
+++ src/Account_projet_poo/PetitMenu.java
@@ -73,9 +73,9 @@ public class PetitMenu {
 
 	}
 
-	public Boolean ouiOrNon() {
+	public Boolean ouiOrNon(int nbTexte) {
 		Scanner ouiOrNon = new Scanner(System.in);
-		System.out.println("Vous voulez modifier le " + Contact.texte[1] + "? O/N");
+		System.out.println("Vous voulez modifier le " + Contact.texte[nbTexte] + "? O/N");
 		String ouiOrNonString = ouiOrNon.nextLine();
 		boolean a = false;
 		if (ouiOrNonString.equals("o") == true || ouiOrNonString.equals("O") == true)
@@ -88,46 +88,57 @@ public class PetitMenu {
 
 	public Array_user modificationContact(Array_user contacts) {
 		int indice;
+
 		System.out.println("Nom a modifier:");
 		String nomIndiceString = scannerUser.nextLine();
 		indice = contacts.findString(nomIndiceString);
-
+		String adresse = contacts.tableauContact[indice].getAdresse();
+		String profession = contacts.tableauContact[indice].getProfession();
 		System.out.println("Modification contact " + Contact.texte[0] + contacts.tableauContact[indice].getNom());
 		String nom = contacts.tableauContact[indice].getNom();
 
-		if (ouiOrNon() == true) {
+		if (ouiOrNon(1) == true) {
 			prenoms = addInfo(prenoms, Contact.texte[1]);
-		} else if (ouiOrNon() == false) {
+		} else if (ouiOrNon(1) == false) {
 			prenoms = contacts.tableauContact[indice].getPrenom();
 		}
 
-		if (ouiOrNon() == true) {
-			String adresse = scannerUser.nextLine();
-		} else if (ouiOrNon() == false) {
-			String adresse = contacts.tableauContact[indice].getAdresse();
+		if (ouiOrNon(2) == true) {
+			adresse = scannerUser.nextLine();
+		} else {
+
 		}
-		if (ouiOrNon() == true) {
+
+		if (ouiOrNon(3) == true) {
 			telephone = addInfo(telephone, Contact.texte[3]);
-		} else if (ouiOrNon() == false) {
+		} else if (ouiOrNon(3) == false) {
 			telephone = contacts.tableauContact[indice].getTelephone();
 		}
 
-		if (ouiOrNon() == true) {
-
+		if (ouiOrNon(4) == true) {
 			email = addInfo(email, Contact.texte[4]);
-		} else if (ouiOrNon() == false) {
+		} else if (ouiOrNon(4) == false) {
 			email = contacts.tableauContact[indice].getEmail();
 
 		}
 
-		reseauxSociaux = addInfo(reseauxSociaux, Contact.texte[5]);
+		if (ouiOrNon(5) == true) {
+			reseauxSociaux = addInfo(reseauxSociaux, Contact.texte[5]);
+		} else if (ouiOrNon(5) == false) {
+			reseauxSociaux = contacts.tableauContact[indice].getReseauxSociaux();
+		}
 
-		System.out.println(Contact.texte[6]);
-		String profession = scannerUser.nextLine();
+		if (ouiOrNon(6) == true) {
+			System.out.println(Contact.texte[6]);
+			profession = scannerUser.nextLine();
+		} else {
+
+		}
 
 		Contact nouveauContact = new Contact(nom, prenoms, adresse, telephone, email, reseauxSociaux, profession);
 
 		contacts.remplaceContact(nouveauContact, indice);
+
 		return contacts;
 
 	}
